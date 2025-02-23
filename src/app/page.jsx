990b9f6c0c5efa9d2636e16b1eef6ee61d3e7e73
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-02-23 14:05:28 -0500

    styles particles and hero page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,51 @@
 "use client"
 
+import { useMemo, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 import Hero from "@/components/Hero"
 import TaskList from "@/components/TaskList"
-import { useMemo } from "react";
-import Particles from "@tsparticles/react"
 
 export default function Home() {
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
 
     const options = useMemo(
         () => ({
-            fpsLimit: 60,
-            // interactivity: {
-            //     events: {
-            //         onClick: {
-            //             enable: true,
-            //             mode: "push",
-            //         },
-            //         onHover: {
-            //             enable: true,
-            //             mode: "repulse",
-            //         },
-            //     },
-            //     modes: {
-            //         push: {
-            //             quantity: 4,
-            //         },
-            //         repulse: {
-            //             distance: 200,
-            //             duration: 0.4,
-            //         },
-            //     },
-            // },
+            fpsLimit: 240,
+            interactivity: {
+                events: {
+                    onHover: {
+                        enable: true,
+                        mode: "repulse",
+                        parallax: {
+                            enable: true,
+                            force: 60,
+                            smooth: 10,
+                        }
+                    }
+                },
+                modes: {
+                    repulse: {
+                        distance: 500,
+                        duration: 4,
+                        factor: 1,
+                        speed: 1,
+                        maxSpeed: 5,
+                        easing: "ease-out-quad",
+                    },
+                },
+            },
             particles: {
                 color: {
                     value: "#cfbafc",
@@ -42,17 +57,17 @@ export default function Home() {
                         default: "bounce",
                     },
                     random: false,
-                    speed: 1,
+                    speed: 0.5,
                     straight: false,
                 },
                 number: {
                     density: {
                         enable: true,
                     },
-                    value: 10,
+                    value: 50,
                 },
                 opacity: {
-                    value: 0.1,
+                    value: 0.25,
                 },
                 shape: {
                     type: "circle",
@@ -61,16 +76,17 @@ export default function Home() {
                     value: { min: 1, max: 3 },
                 },
             },
-            // detectRetina: true,
         }),
         [],
     );
-	return (
-		<div className="bg-black text-white">
-			<Hero />
-            <Particles options={options} />
-			{/* <TaskList /> */}
-			{/* Hello, World! */}
-		</div>
-	)
+
+    if (init) {
+        return (
+            <div className="bg-black text-white">
+                <Particles options={options} />
+                <Hero />
+                <TaskList />
+            </div>
+        )
+    }
 }
