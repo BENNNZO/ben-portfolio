8c122364f63e36ae27c6aa3457571a6dda04c9a5
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-02-22 19:46:56 -0500

    styles hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,19 @@ import Image from "next/image"
 export default function Hero() {
     return (
         <section className="h-screen bg-black flex flex-row justify-around items-center">
-            <div>
-                <p className="font-bold text-6xl">HEY, I'M BEN!</p>
-                <p className="font-semibold text-3xl opacity-50">JACK OF ALL TRAITS</p>
+            <div className="relative flex flex-col gap-2">
+                <p className="font-bold text-6xl">HEY, I'M <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-purple-600">BEN</span>!</p>
+                <p className="font-semibold text-3xl opacity-50">JACK OF <span className="underline">ALLLL</span> TRAITS</p>
+                <div className="w-24 h-8 bg-purple-400 blur-xl absolute top-3 right-8 opacity-50"></div>
             </div>
-            <Image src="/img/profile.png" alt="Profile Picture" width={500} height={500} className="rounded-full" />
+            <div className="relative w-96 h-96 rounded-full">
+                <div className="bg-purple-700 absolute-center w-44 h-44 blur-3xl opacity-50"></div>
+                <Image src="/img/profile.png" alt="Profile Picture" width={500} height={500} className="w-full h-full aspect-square object-cover scale-110 bottom-2 left-2 absolute drop-shadow-lg rounded-full blur-lg opacity-70" />
+                <Image src="/img/profile.png" alt="Profile Picture" width={500} height={500} className="w-full h-full aspect-square object-cover scale-110 bottom-2 left-2 relative drop-shadow-lg rounded-full" />
+                {/* <div className="bg-red-500 absolute-center w-2 h-2"></div> */}
+            </div>
         </section>
     )
 }
+
+// bg-gradient-to-t from-purple-500 to-purple-800 to-100%
