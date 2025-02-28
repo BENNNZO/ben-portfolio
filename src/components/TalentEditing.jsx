c8ae5ab8cc95c390229a6ed1cc360d8eebe3acb5
Benjamin Phillips <benjamin.phillips.webdev@gmail.com>
2025-02-28 15:35:24 -0600

    added my videos to the editing section

--- src/components/TalentEditing.jsx
+++ src/components/TalentEditing.jsx
@@ -1,28 +1,30 @@
+import Image from "next/image"
+
 export default function TalentEditing() {
     const videos = [
         {
             title: "I Made Cookie Clicker In MINECRAFT!",
-            url: "https://www.youtube.com/watch?v=Ax3II-K9_0U"
+            url: "https://www.youtube.com/embed/Ax3II-K9_0U"
         },
         {
-            title: "I Made Cookie Clicker In MINECRAFT!",
-            url: "https://www.youtube.com/watch?v=Ax3II-K9_0U"
+            title: "I Broke Minecraft 10 Times...",
+            url: "https://www.youtube.com/embed/O1XRC0rrETg"
         },
         {
-            title: "I Made Cookie Clicker In MINECRAFT!",
-            url: "https://www.youtube.com/watch?v=Ax3II-K9_0U"
+            title: "So I Deep Fried Minecraft...",
+            url: "https://www.youtube.com/embed/2PUSGN3vOm8"
         },
         {
-            title: "I Made Cookie Clicker In MINECRAFT!",
-            url: "https://www.youtube.com/watch?v=Ax3II-K9_0U"
+            title: "Minecraft But I'm TINY...",
+            url: "https://www.youtube.com/embed/rElYAUbA3wk"
         },
         {
-            title: "I Made Cookie Clicker In MINECRAFT!",
-            url: "https://www.youtube.com/watch?v=Ax3II-K9_0U"
+            title: "I Built HOTDOG PARADRISE In Survival Minecraft!",
+            url: "https://www.youtube.com/embed/jL9MS53vvuw"
         },
         {
-            title: "I Made Cookie Clicker In MINECRAFT!",
-            url: "https://www.youtube.com/watch?v=Ax3II-K9_0U"
+            title: "When The Liar Hides In Plain Sight",
+            url: "https://www.youtube.com/embed/tU8L7bRarBg"
         }
     ]
 
@@ -30,15 +32,22 @@ export default function TalentEditing() {
         <>
 
             {/* PROJECTS */}
-            <h3 className="text-shine-purple text-4xl font-bold lg:pl-2 whitespace-nowrap text-center lg:text-left mt-8 lg:mt-0">VIDEOS</h3>
-            <div className="md:grid md:grid-cols-2 md:grid-rows-3 gap-2 my-4 flex flex-col relative">
+            {/* <h3 className="text-shine-purple text-4xl font-bold whitespace-nowrap text-center lg:text-left mt-8 lg:mt-0">BANNER</h3> */}
+            {/* <h3 className="text-shine-purple text-4xl font-bold whitespace-nowrap text-center lg:text-left mt-4">VIDEOS</h3> */}
+            <div className="relative">
+                <Image src="/img/channel banner.jpg" alt="channel banner" className="rounded-md" width={1024} height={500} />
+            </div>
+            <div className="md:grid md:grid-cols-2 md:grid-rows-3 gap-2 my-2 flex flex-col relative">
                 {videos.map((video, index) => (
-                    <iframe key={index} className="aspect-video w-full rounded-md" src={video.url} title="YouTube video player" allowfullscreen></iframe>
-                    // <video key={index} src={video.url} className="bg-red-400 w-24 aspect-video"></video>
-                    // <div key={index} className="aspect-[3/2] relative rounded-md group lg:scale-95 lg:hover:scale-100 duration-200 ease-out">
-                        // <video src={video.source} width={1920} height={1080} className="h-full w-full object-top object-cover rounded-md absolute duration-500 ease-out blur-xl group-hover:blur-2xl opacity-50 group-hover:opacity-75" />
-                        // <video src={video.source} width={1920} height={1080} className="h-full w-full object-top object-cover rounded-md absolute duration-500 ease-out opacity-100" />
-                    // </div>
+                    <iframe
+                        key={index}
+                        src={video.url}
+
+                        title={video.title} 
+                        allowFullScreen
+                        className="aspect-video w-full rounded-md" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    ></iframe>
                 ))}
             </div>
         </>
