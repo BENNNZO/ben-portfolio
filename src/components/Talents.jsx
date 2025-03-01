c319dad41873e6833bee21e8744f3c28e326b773
Benjamin Phillips <benjamin.phillips.webdev@gmail.com>
2025-03-01 13:36:37 -0600

    removed currently unfinished sections

--- src/components/Talents.jsx
+++ src/components/Talents.jsx
@@ -19,10 +19,10 @@ export default function Talents() {
         <section className="w-full relative flex flex-col items-center">
             <Header title="TALENTS" />
             <div className="flex flex-row flex-wrap justify-center gap-2 mb-12">
-                <div className="relative">
+                {/* <div className="relative">
                     <button className={`border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "cyber" ? "bg-black text-shine-purple border-purple-300 scale-100" : "border-white/30 scale-90 opacity-40"}`} onClick={() => setSelectedTalent("cyber")}>Cyber</button>
                     <button className={`absolute top-0 left-0 border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "cyber" ? "bg-black text-shine-purple border-purple-300 scale-100 blur-lg" : "border-white/30 scale-90 opacity-0"}`} onClick={() => setSelectedTalent("cyber")}>Cyber</button>
-                </div>
+                </div> */}
                 <div className="relative">
                     <button className={`border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "coder" ? "bg-black text-shine-purple border-purple-300 scale-100" : "border-white/30 scale-90 opacity-40"}`} onClick={() => setSelectedTalent("coder")}>Coding</button>
                     <button className={`absolute top-0 left-0 border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "coder" ? "bg-black text-shine-purple border-purple-300 scale-100 blur-lg" : "border-white/30 scale-90 opacity-0"}`} onClick={() => setSelectedTalent("coder")}>Coding</button>
@@ -32,18 +32,18 @@ export default function Talents() {
                     <button className={`absolute top-0 left-0 border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "music" ? "bg-black text-shine-purple border-purple-300 scale-100 blur-lg" : "border-white/30 scale-90 opacity-0"}`} onClick={() => setSelectedTalent("music")}>Music</button>
                 </div>
                 <div className="relative">
-                    <button className={`border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "desin" ? "bg-black text-shine-purple border-purple-300 scale-100" : "border-white/30 scale-90 opacity-40"}`} onClick={() => setSelectedTalent("desin")}>Design</button>
-                    <button className={`absolute top-0 left-0 border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "desin" ? "bg-black text-shine-purple border-purple-300 scale-100 blur-lg" : "border-white/30 scale-90 opacity-0"}`} onClick={() => setSelectedTalent("desin")}>Design</button>
-                </div>
-                <div className="relative">
                     <button className={`border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "edits" ? "bg-black text-shine-purple border-purple-300 scale-100" : "border-white/30 scale-90 opacity-40"}`} onClick={() => setSelectedTalent("edits")}>Editing</button>
                     <button className={`absolute top-0 left-0 border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "edits" ? "bg-black text-shine-purple border-purple-300 scale-100 blur-lg" : "border-white/30 scale-90 opacity-0"}`} onClick={() => setSelectedTalent("edits")}>Editing</button>
                 </div>
+                {/* <div className="relative">
+                    <button className={`border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "desin" ? "bg-black text-shine-purple border-purple-300 scale-100" : "border-white/30 scale-90 opacity-40"}`} onClick={() => setSelectedTalent("desin")}>Design</button>
+                    <button className={`absolute top-0 left-0 border-2 rounded-full px-3 py-0.5 duration-200 ease-out ${selectedTalent === "desin" ? "bg-black text-shine-purple border-purple-300 scale-100 blur-lg" : "border-white/30 scale-90 opacity-0"}`} onClick={() => setSelectedTalent("desin")}>Design</button>
+                </div> */}
             </div>
             <div className="flex justify-center">
-                <div className={`absolute max-w-5xl w-full ease-in-out duration-300 ${selectedTalent === "cyber" ? "opacity-100 blur-0 scale-100" : "opacity-0 blur-md scale-90 pointer-events-none -translate-x-0"}`}>
+                {/* <div className={`absolute max-w-5xl w-full ease-in-out duration-300 ${selectedTalent === "cyber" ? "opacity-100 blur-0 scale-100" : "opacity-0 blur-md scale-90 pointer-events-none -translate-x-0"}`}>
                     <TalentCyber />
-                </div>
+                </div> */}
                 <div className={`relative max-w-5xl w-full ease-in-out duration-300 ${selectedTalent === "coder" ? "opacity-100 blur-0 scale-100" : "opacity-0 blur-md scale-95 pointer-events-none translate-y-0"}`}>
                     <TalentCoding />
                 </div>
@@ -51,11 +51,11 @@ export default function Talents() {
                     <TalentMusic />
                 </div>
                 <div className={`absolute max-w-5xl w-full ease-in-out duration-300 ${selectedTalent === "edits" ? "opacity-100 blur-0 scale-100" : "opacity-0 blur-md scale-90 pointer-events-none translate-x-0"}`}>
-                    <TalentCyber />
-                </div>
-                <div className={`absolute max-w-5xl w-full ease-in-out duration-300 ${selectedTalent === "desin" ? "opacity-100 blur-0 scale-100" : "opacity-0 blur-md scale-90 pointer-events-none translate-x-0"}`}>
                     <TalentEditing />
                 </div>
+                {/* <div className={`absolute max-w-5xl w-full ease-in-out duration-300 ${selectedTalent === "desin" ? "opacity-100 blur-0 scale-100" : "opacity-0 blur-md scale-90 pointer-events-none translate-x-0"}`}>
+                    <TalentEditing />
+                </div> */}
             </div>
         </section>
     )
