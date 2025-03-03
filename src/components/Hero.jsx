046fdc04b585d7d320ec18fdb4b85898770bd54c
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-03-03 16:25:08 -0500

    added max-w to hero blur bubbles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ export default function Hero() {
     return (
         <section className="h-screen flex lg:flex-row flex-col-reverse lg:justify-around justify-center lg:gap-0 gap-24 items-center overflow-hidden relative hero-box-shadow">
             {/* BUBBLES */}
-            <div className="bg-purple-400 absolute bottom-10 left-1/2 -translate-x-1/2 w-[50%] aspect-[2/1] rounded-t-full hero-pulse" style={{ filter: 'blur(300px)' }}></div>
-            <div className="bg-fuchsia-400 absolute bottom-10 left-1/2 -translate-x-1/2 w-full h-52 rounded-t-full blur-[300px]"></div>
+            <div className="bg-purple-400 absolute bottom-10 left-1/2 -translate-x-1/2 w-[50%] max-w-5xl aspect-[2/1] rounded-t-full hero-pulse" style={{ filter: 'blur(300px)' }}></div>
+            <div className="bg-fuchsia-500 absolute bottom-10 left-1/2 -translate-x-1/2 w-full max-w-[100rem] h-52 rounded-t-full blur-[300px]"></div>
 
             <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full h-52 bg-gradient-to-b from-transparent to-black blur-xl"></div>
             
@@ -16,8 +16,9 @@ export default function Hero() {
                 <div className="w-24 h-8 bg-purple-400 blur-xl absolute top-3 right-8 md:opacity-50 opacity-0"></div>
             </div>
 
-            <div className="relative w-96 h-96 rounded-full">
-                <div className="bg-purple-700 absolute-center w-44 h-44 blur-3xl opacity-50"></div>
+            <div className="relative md:w-96 md:h-96 w-72 h-72 rounded-full md:bottom-16">
+                <div className="bg-purple-900 absolute-center w-44 h-44 blur-3xl opacity-50"></div>
+                {/* <div className="bg-fuchsia-700 absolute-center w-44 h-44 blur-3xl opacity-50"></div> */}
                 <Image src="/img/profile.png" alt="Profile Picture" width={500} height={500} className="w-full h-full aspect-square object-cover scale-110 bottom-2 left-2 absolute drop-shadow-lg rounded-full blur-lg opacity-70" />
                 <Image src="/img/profile.png" alt="Profile Picture" width={500} height={500} className="w-full h-full aspect-square object-cover scale-110 bottom-2 left-2 relative drop-shadow-lg rounded-full" />
             </div>
