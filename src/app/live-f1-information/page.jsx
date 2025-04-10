de0a7e6c73123eddc7e47c4fa827829c0e21d63c
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-10 09:59:45 -0400

    created new page

---
+++ src/app/live-f1-information/page.jsx
@@ -0,0 +1,93 @@
+"use client"
+
+import { useMemo, useEffect, useState } from "react";
+import { loadSlim } from "@tsparticles/slim";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+
+export default function Home() {
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const options = useMemo(
+        () => ({
+            fpsLimit: 240,
+            interactivity: {
+                events: {
+                    onHover: {
+                        enable: true,
+                        mode: "bubble",
+                        parallax: {
+                            enable: true,
+                            force: 60,
+                            smooth: 10,
+                        }
+                    }
+                },
+                modes: {
+                    repulse: {
+                        distance: 500,
+                        duration: 4,
+                        factor: 1,
+                        speed: 1,
+                        maxSpeed: 5,
+                        easing: "ease-out-quad",
+                    },
+                },
+            },
+            particles: {
+                color: {
+                    value: "#cfbafc",
+                },
+                move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: {
+                        default: "out",
+                    },
+                    random: false,
+                    speed: 0.5,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: true,
+                    },
+                    value: 50,
+                },
+                opacity: {
+                    value: { min: 0.01, max: 0.1 },
+                },
+                shape: {
+                    type: "circle",
+                },
+                size: {
+                    value: { min: 2, max: 4 },
+                    random: true,
+                },
+            },
+            detectRetina: true,
+        }),
+        [],
+    );
+
+    if (init) {
+        return (
+            <div className="bg-black text-white min-h-screen">
+                <Particles options={options} />
+                <div className="px-4 md:px-8 lg:px-0">
+                    hello world
+                </div>
+            </div>
+        )
+    }
+}
