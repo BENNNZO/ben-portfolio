abd68f6a1b5047c7651f9c7e8b6053b54f32e7ac
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-11 14:35:59 -0400

    minor style changes

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,8 +7,8 @@ export default function Contacts() {
                 <Header title="CONTACTS" />
                 <p className="animate-pulse mb-12">E-Mail form coming soon...</p>
                 <div className="border border-purple-400/20 bg-purple-950/5 rounded-md mb-32 p-4 backdrop-blur-sm">
-                    <p className="mb-4 text-sm md:text-md">NOTE - I get a lot of spam calls so If I don't answer a call don't hesitate to text or e-mail me!</p>
-                    <div className="flex flex-col gap-2">
+                    <p className="mb-4 text-sm md:text-md">I may miss calls so feel free to text, e-mail or leave a voice mail!</p>
+                    <div className="flex flex-row justify-between gap-2 md:gap-8 flex-wrap">
                         <div className="flex flex-row gap-2">
                             <Image src="/svg/call.svg" width={25} height={25} alt="phone icon" className="invert" />
                             <a className="md:text-xl text-sm font-semibold text-shine-purple" href="tel:+17068300401">+1 (706) 830 - 0401</a>
