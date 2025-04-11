f2f3ef7d22cb262e2540fdfc1e3fb9ec4a70783d
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-11 14:32:43 -0400

    removed my dev email from contacts because I just don't check it that much anymore

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -17,10 +17,6 @@ export default function Contacts() {
                             <Image src="/svg/mail.svg" width={25} height={25} alt="phone icon" className="invert" />
                             <a className="md:text-xl text-sm font-semibold text-shine-purple" href="mailto:bp08262004@gmail.com">bp08262004@gmail.com</a>
                         </div>
-                        <div className="flex flex-row gap-2">
-                            <Image src="/svg/mail.svg" width={25} height={25} alt="phone icon" className="invert" />
-                            <a className="md:text-xl text-sm font-semibold text-shine-purple" href="mailto:benjamin.phillips.webdev@gmail.com">benjamin.phillips.webdev@gmail.com</a>
-                        </div>
                     </div>
                 </div>
             </section>
